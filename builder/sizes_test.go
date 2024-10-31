47f5d3c1d2b54ed2fea7a06e5040d0c484a587fc
HattoriHanzo031 <filip031@gmail.com>
2024-10-31 01:10:46 +0100

    test binary size update

--- builder/sizes_test.go
+++ builder/sizes_test.go
@@ -41,7 +41,7 @@ func TestBinarySize(t *testing.T) {
 	// This is a small number of very diverse targets that we want to test.
 	tests := []sizeTest{
 		// microcontrollers
-		{"hifive1b", "examples/echo", 4688, 280, 0, 2268},
+		{"hifive1b", "examples/echo", 4692, 280, 0, 2268},
 		{"microbit", "examples/serial", 2952, 388, 8, 2272},
 		{"wioterminal", "examples/pininterrupt", 6148, 1484, 116, 6832},
 
